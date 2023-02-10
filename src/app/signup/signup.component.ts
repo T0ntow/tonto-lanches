5a564dc213025c37a777c4815f55429898ac3510
Wellington Batista <wellingtonbs109@gmail.com>
2023-02-10 16:11:01 -0300

    update

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
 
 interface CheckboxItem {
@@ -15,17 +16,21 @@ interface CheckboxItem {
 })
 export class SignupComponent implements OnInit {
   signupForm: FormGroup = new FormGroup({});
+  // selectedType = '';
+
   constructor(
     private formBuilder: FormBuilder,
     private fireAuth: AngularFireAuth,
-    private router: Router
+    private firestore: AngularFirestore,
+    private router: Router,
   ) { }
 
   ngOnInit() {
     this.signupForm = this.formBuilder.group({
       user: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      // checkbox: [false, [this.onCheckboxChange]]
     });
   }
 
@@ -34,31 +39,19 @@ export class SignupComponent implements OnInit {
       this.fireAuth.createUserWithEmailAndPassword(this.signupForm.value.email, this.signupForm.value.password)
         .then((userCredential) => {
           if (userCredential.user) {
-            userCredential.user.updateProfile({
-              displayName: this.signupForm.value.user
-            });
-            this.router.navigate(['/home']);
 
-            console.log('name: ' + this.signupForm.value.user);
+            this.firestore.collection('users').add({
+              userName: this.signupForm.value.user,
+              email: this.signupForm.value.email,
+            });
+
+            alert("Conta criada com sucesso seja bem vindo " + this.signupForm.value.user)
+            this.router.navigate(['/home']);
           }
         })
         .catch((error) => {
           console.error(error);
         });
     }
-  }
- 
-  items = [
-    { label: 'Option 1', checked: false },
-    { label: 'Option 2', checked: false },
-  ];
-  onCheckboxChange(selectedItem: CheckboxItem) {
-    this.items.forEach(item => {
-      if (item === selectedItem) {
-        item.checked = !item.checked;
-      } else {
-        item.checked = false;
-      }
-    });
   }
 }
